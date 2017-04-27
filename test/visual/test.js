e228a575925b03ec8ceee7da2033b16ad6033a51
Limon Monte <limon.monte@gmail.com>
2017-04-27 20:15:40 +0300

    Fix Firefox gemini tests sukablya

--- test/visual/test.js
+++ test/visual/test.js
@@ -2,12 +2,19 @@ gemini.suite('vaadin-text-field', function(rootSuite) {
   function wait(actions, find) {
     actions.wait(5000);
   }
+  function goToAboutBlank(actions, find) {
+    // Firefox stops responding on socket after a test, workaround:
+    return actions.executeJS(function(window) {
+      window.location.href = 'about:blank'; // just go away, please!
+    });
+  }
 
   gemini.suite('default-tests', function(suite) {
     suite
       .setUrl('/default.html')
       .setCaptureElements('#default-tests')
-      .capture('default', {}, wait);
+      .capture('default', {}, wait)
+      .after(goToAboutBlank);
   });
 
   gemini.suite('control-state', function(suite) {
@@ -17,14 +24,16 @@ gemini.suite('vaadin-text-field', function(rootSuite) {
       .capture('control-state-focused', {}, wait)
       .capture('control-state-focus-ring', function(actions, find) {
         actions.sendKeys(gemini.TAB);
-      });
+      })
+      .after(goToAboutBlank);
   });
 
   gemini.suite('styling', function(suite) {
     suite
       .setUrl('styling.html')
       .setCaptureElements('vaadin-text-field')
-      .capture('default', {}, wait);
+      .capture('default', {}, wait)
+      .after(goToAboutBlank);
   });
 
 });
