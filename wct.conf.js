9a379b1d3675df82f51dcbdc4cd34297e5343566
web-padawan <serguey.kulikov@gmail.com>
2018-08-02 12:55:37 +0300

    Update wct config to fix Safari timeouts

--- wct.conf.js
+++ wct.conf.js
@@ -3,12 +3,12 @@ var argv = require('yargs').argv;
 module.exports = {
   registerHooks: function(context) {
     var saucelabsPlatforms = [
-      'macOS 10.12/iphone@10.3',
-      'macOS 10.12/ipad@10.3',
-      'Windows 10/microsoftedge@15',
+      'iOS Simulator/iphone@11.3',
+      'iOS Simulator/ipad@10.3',
+      'Windows 10/microsoftedge@17',
       'Windows 10/internet explorer@11',
-      'macOS 10.12/safari@11.0',
-      'macOS 9.3.2/iphone@9.3'
+      'macOS 10.13/safari@11.1',
+      'iOS Simulator/iphone@9.3'
     ];
 
     var cronPlatforms = [
