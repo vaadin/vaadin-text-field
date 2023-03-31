413016f9f56eb40d16793f18fa5d78b0679b15c4
J. Diogo Oliveira <jdso@vhelsi.com>
2023-03-31 04:31:12 +0100

    tests - validation tests separated

--- test/test-suites.js
+++ test/test-suites.js
@@ -7,5 +7,11 @@ window.VaadinTextFieldSuites = [
   'accessibility.html',
   'custom-input.html',
   'email-field.html',
-  'integer-field.html'
+  'integer-field.html',
+  'text-field-validation.html',
+  'text-area-validation.html',
+  'number-field-validation.html',
+  'integer-field-validation.html',
+  'password-field-validation.html',
+  'email-field-validation.html'
 ];
