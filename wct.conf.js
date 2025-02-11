e04e49fb2aa991adc955ad65c8fded0d8f3b3ed1
Sascha Ißbrücker <sissbruecker@vaadin.com>
2025-02-11 14:09:28 +0100

    chore: update sauce connect launcher (#638)

--- wct.conf.js
+++ wct.conf.js
@@ -1,6 +1,11 @@
 var envIndex = process.argv.indexOf('--env') + 1;
 var env = envIndex ? process.argv[envIndex] : undefined;
 
+// workaround for Android 7+ blocking all HTTP traffic
+// see https://wiki.saucelabs.com/display/DOCS/Known+Issues
+// add it to your own local `/etc/hosts` to run SauceLabs tests locally
+var tunneledLocalhost = 'localhost-for-saucelabs';
+
 module.exports = {
   testTimeout: 180 * 1000,
   verbose: false,
@@ -14,55 +19,41 @@ module.exports = {
         ]
       }
     },
-    // MAGI REMOVE START
-    istanbul: {
-      dir: './coverage',
-      reporters: ['text-summary', 'lcov'],
-      include: [
-        '**/vaadin-text-field/src/*.html'
-      ],
-      exclude: [],
-      thresholds: {
-        global: {
-          statements: 87
-        }
-      }
-    }
-    // MAGI REMOVE END
   },
 
   registerHooks: function(context) {
-    const saucelabsPlatformsMobile = [
-      'iOS Simulator/iphone@12.2',
-      'iOS Simulator/iphone@10.3'
-    ];
-
-    const saucelabsPlatformsMicrosoft = [
-      'Windows 10/microsoftedge@18',
-      'Windows 10/internet explorer@11'
-    ];
-
-    const saucelabsPlatformsDesktop = [
-      'macOS 10.13/safari@latest'
-    ];
-
-    const saucelabsPlatforms = [
-      ...saucelabsPlatformsMobile,
-      ...saucelabsPlatformsMicrosoft,
-      ...saucelabsPlatformsDesktop
-    ];
-
-    const cronPlatforms = [
-      'iOS Simulator/ipad@12.2',
-      'iOS Simulator/iphone@10.3',
+    const testBrowsers = [
+      {
+        deviceName: 'Android GoogleAPI Emulator',
+        platformName: 'Android',
+        platformVersion: '11.0',
+        browserName: 'Chrome',
+      },
+      'iOS Simulator/iphone@10.3', // should be 9.x, but SauceLabs does not provide that
+      'macOS 11/safari@latest',
+      'Windows 10/microsoftedge@latest',
+      'Windows 10/internet explorer@11',
       'Windows 10/chrome@latest',
-      'Windows 10/firefox@latest'
+      'Windows 10/firefox@latest',
     ];
 
     if (env === 'saucelabs') {
-      context.options.plugins.sauce.browsers = saucelabsPlatforms;
-    } else if (env === 'saucelabs-cron') {
-      context.options.plugins.sauce.browsers = cronPlatforms;
+      context.options.webserver = context.options.webserver || {};
+      context.options.webserver.hostname = tunneledLocalhost;
+      context.options.plugins.sauce.tunnelOptions = {
+        tunnelDomains: tunneledLocalhost
+      };
+
+      context.options.plugins.sauce.browsers = testBrowsers;
     }
+
+    // Map legacy tunnel-identifier option to new tunnel-name option
+    context.hookLate('prepare', (done) => {
+      context.options.activeBrowsers.forEach((browser) => {
+        browser['tunnel-name'] = browser['tunnel-identifier'];
+        delete browser['tunnel-identifier'];
+      });
+      done();
+    });
   }
 };
